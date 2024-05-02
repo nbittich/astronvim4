612b0790a306249f5241f2c9bd7912d86da1f2ce
Nordine Bittich <nordine@bittich.be>
2024-05-02 23:46:30 +0200

    missing crates plugin

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -22,6 +22,37 @@ return {
     },
   },
   {
+    "Saecki/crates.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          CmpSourceCargo = {
+            {
+              event = "BufRead",
+              desc = "Load crates.nvim into Cargo buffers",
+              pattern = "Cargo.toml",
+              callback = function()
+                require("cmp").setup.buffer { sources = { { name = "crates" } } }
+                require "crates"
+              end,
+            },
+          },
+        },
+      },
+    },
+    opts = {
+      src = {
+        cmp = { enabled = true },
+      },
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+    },
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = { "rust_analyzer" }, -- automatically install lsp
